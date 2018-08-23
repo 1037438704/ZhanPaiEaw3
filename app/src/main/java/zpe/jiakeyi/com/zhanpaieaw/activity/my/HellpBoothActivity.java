d0a84fc1de43f5eee63a4618401553821fc28096
gjf <1321114941@qq.com>
2018-08-23 12:49:51 +0800

    2

--- app/src/main/java/zpe/jiakeyi/com/zhanpaieaw/activity/my/HellpBoothActivity.java
+++ app/src/main/java/zpe/jiakeyi/com/zhanpaieaw/activity/my/HellpBoothActivity.java
@@ -1,18 +1,18 @@
 package zpe.jiakeyi.com.zhanpaieaw.activity.my;
 
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.kongzue.baseframework.BaseActivity;
-import com.kongzue.baseframework.interfaces.DarkNavigationBarTheme;
 import com.kongzue.baseframework.interfaces.DarkStatusBarTheme;
 import com.kongzue.baseframework.interfaces.Layout;
-import com.kongzue.baseframework.interfaces.NavigationBarBackgroundColor;
 import com.kongzue.baseframework.util.JumpParameter;
 
 import zpe.jiakeyi.com.zhanpaieaw.R;
+
 /**
- *
  * @author Gjianfu
  * @date 2018/8/13
  * 功能描述:
@@ -21,11 +21,21 @@ import zpe.jiakeyi.com.zhanpaieaw.R;
 @DarkStatusBarTheme(true)
 public class HellpBoothActivity extends BaseActivity {
     private ImageView title_back;
+    private TextView title_name;
+    private ImageView title_seek;
+    private TextView title_text_right;
+    private TextView title;
+    private TextView content_tv;
 
     @Override
     public void initViews() {
         title_back = findViewById(R.id.title_back);
-
+        title_name = findViewById(R.id.title_name);
+        title_seek = findViewById(R.id.title_seek);
+        title_seek.setVisibility(View.GONE);
+        title_text_right = findViewById(R.id.title_text_right);
+        title_text_right.setVisibility(View.VISIBLE);
+        title_name.setText("帮助中心");
     }
 
     @Override
@@ -39,6 +49,12 @@ public class HellpBoothActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        title_text_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
